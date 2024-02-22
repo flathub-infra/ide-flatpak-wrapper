f269b6d576e485fd0e627e953c498423ef14fba3
noons <noons@riseup.net>
2024-02-22 16:12:14 -0300

    :hammer: Wayland support for electron apps

--- editor.sh
+++ editor.sh
@@ -7,9 +7,25 @@ shopt -s nullglob
 FIRST_RUN="${XDG_CONFIG_HOME}/@FLAGFILE_PREFIX@-first-run"
 SDK_UPDATE="${XDG_CONFIG_HOME}/@FLAGFILE_PREFIX@-sdk-update-@SDK_VERSION@"
 
+function display_server_args (){
+  # See https://github.com/flathub/im.riot.Riot/blob/3fdd41c84f40fa1e8e186bade5d832d79045600c/element.sh
+  if [ "wayland" == "${XDG_SESSION_TYPE}" ] && [ -e "${XDG_RUNTIME_DIR}/${WAYLAND_DISPLAY}" ]
+  then
+      DISPLAY_SERVER_ARGS="--ozone-platform-hint=auto --enable-wayland-ime --enable-features=WaylandWindowDecorations"
+      if  [ -c /dev/nvidia0 ]
+      then
+          DISPLAY_SERVER_ARGS="${DISPLAY_SERVER_ARGS} --disable-gpu-sandbox" 
+      fi
+  else
+      DISPLAY_SERVER_ARGS="--ozone-platform=x11"
+  fi
+  echo "${DISPLAY_SERVER_ARGS}"
+}
+
 function exec_editor() {
   @EXPORT_ENVS@
-  exec "@WRAPPER_PATH@" @EDITOR_ARGS@ "$@"
+  # shellcheck disable=SC2046
+  exec "@WRAPPER_PATH@" @EDITOR_ARGS@ $(display_server_args) "$@"
 }
 
 if [ ! -f "${FIRST_RUN}" ]; then
