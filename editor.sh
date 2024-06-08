1671bc07d31b2663e52eea748482c0d8079db4bf
noons <noons@riseup.net>
2024-06-08 15:45:24 -0300

    :wrench: match the existing format

--- editor.sh
+++ editor.sh
@@ -11,18 +11,15 @@ function display_server_args (){
   # See https://github.com/flathub/im.riot.Riot/blob/3fdd41c84f40fa1e8e186bade5d832d79045600c/element.sh
   # See also https://gaultier.github.io/blog/wayland_from_scratch.html 
   # and https://github.com/flathub/com.vscodium.codium/issues/321
-  if [ "wayland" == "${XDG_SESSION_TYPE}" ] && [ -n "${WAYLAND_DISPLAY}" ]
-  then
-    if [[ "${WAYLAND_DISPLAY}" =~ ^/ ]]
-    then
+  if [ "wayland" == "${XDG_SESSION_TYPE}" ] && [ -n "${WAYLAND_DISPLAY}" ]; then
+    if [[ "${WAYLAND_DISPLAY}" =~ ^/ ]]; then
       wayland_socket="${WAYLAND_DISPLAY}"
     else
       wayland_socket="${XDG_RUNTIME_DIR:-/run/user/${UID}}/${WAYLAND_DISPLAY}"
     fi
   fi
 
-  if [ -e "$wayland_socket" ]
-  then
+  if [ -e "$wayland_socket" ]; then
     DISPLAY_SERVER_ARGS="--ozone-platform=wayland --enable-wayland-ime --enable-features=WaylandWindowDecorations"
     [ -c /dev/nvidia0 ] && DISPLAY_SERVER_ARGS="${DISPLAY_SERVER_ARGS} --disable-gpu-sandbox"
   else
