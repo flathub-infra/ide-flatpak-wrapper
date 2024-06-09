dfc6d2bdaa6f872eaf86ac6581c8035c46a58a34
Filippe LeMarchand <gasinvein@gmail.com>
2024-06-09 12:29:05 +0300

    Use array for display server args

--- editor.sh
+++ editor.sh
@@ -7,7 +7,9 @@ shopt -s nullglob
 FIRST_RUN="${XDG_CONFIG_HOME}/@FLAGFILE_PREFIX@-first-run"
 SDK_UPDATE="${XDG_CONFIG_HOME}/@FLAGFILE_PREFIX@-sdk-update-@SDK_VERSION@"
 
-function display_server_args (){
+declare -a DISPLAY_SERVER_ARGS=()
+
+function update_display_server_args () {
   # See https://github.com/flathub/im.riot.Riot/blob/3fdd41c84f40fa1e8e186bade5d832d79045600c/element.sh
   # See also https://gaultier.github.io/blog/wayland_from_scratch.html 
   # and https://github.com/flathub/com.vscodium.codium/issues/321
@@ -20,19 +22,24 @@ function display_server_args (){
   fi
 
   if [ -e "$wayland_socket" ]; then
-    DISPLAY_SERVER_ARGS="--ozone-platform=wayland --enable-wayland-ime --enable-features=WaylandWindowDecorations"
-    [ -c /dev/nvidia0 ] && DISPLAY_SERVER_ARGS="${DISPLAY_SERVER_ARGS} --disable-gpu-sandbox"
+    DISPLAY_SERVER_ARGS=(
+      "--ozone-platform=wayland"
+      "--enable-wayland-ime"
+      "--enable-features=WaylandWindowDecorations"
+    )
+    if [ -c /dev/nvidia0 ]; then
+      DISPLAY_SERVER_ARGS+=("--disable-gpu-sandbox")
+    fi
   else
-    DISPLAY_SERVER_ARGS="--ozone-platform=x11"
+    DISPLAY_SERVER_ARGS=("--ozone-platform=x11")
   fi
-
-  echo "${DISPLAY_SERVER_ARGS}"
 }
 
 function exec_editor() {
   @EXPORT_ENVS@
-  # shellcheck disable=SC2046,SC2086
-  exec "@WRAPPER_PATH@" @EDITOR_ARGS@ $(display_server_args) ${EDITOR_RUNTIME_ARGS} "$@"
+  update_display_server_args
+  # shellcheck disable=SC2086
+  exec "@WRAPPER_PATH@" @EDITOR_ARGS@ "${DISPLAY_SERVER_ARGS[@]}" ${EDITOR_RUNTIME_ARGS} "$@"
 }
 
 if [ ! -f "${FIRST_RUN}" ]; then
